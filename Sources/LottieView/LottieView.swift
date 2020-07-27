9269cf175d04ffe3cb04c62323d7ba2d88d358e8
Edward Wang <admin@uwwxy.com>
2020-07-27 14:41:25 -0400

    init lottie view

--- Sources/LottieView/LottieView.swift
+++ Sources/LottieView/LottieView.swift
@@ -1,3 +1,95 @@
-struct LottieView {
-    var text = "Hello, World!"
+//
+//  LottieView.swift
+//  LottieTest
+//
+//  Created by Xiangyu Wang on 7/27/20.
+//
+
+import SwiftUI
+import UIKit
+import Lottie
+
+public struct LottieView: UIViewRepresentable {
+    
+    var name: String
+    @Binding var play: Bool
+    var complete: (Bool) -> Void = {_ in}
+    var animationView = AnimationView()
+
+    public init(name: String, play: Binding<Bool>, complete: @escaping (Bool) -> Void = {_ in}) {
+        self.name = name
+        self._play = play
+        self.complete = complete
+    }
+    
+    public func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView()
+
+        animationView.animation = Animation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+
+        return view
+    }
+
+    public func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {
+        if self.play {
+            animationView.play { (result) in
+                if result {
+                    self.play = false
+                }
+                self.complete(result)
+            }
+        } else {
+            animationView.pause()
+        }
+        
+    }
+
 }
+
+public extension LottieView {
+    func setContentMode(mode: UIView.ContentMode) -> LottieView {
+        self.animationView.contentMode = mode
+        return self
+    }
+    
+    func setLoopMode(mode: LottieLoopMode) -> LottieView {
+        self.animationView.loopMode = mode
+        return self
+    }
+    
+    func setSpeed(speed: CGFloat) -> LottieView {
+        self.animationView.animationSpeed = speed
+        return self
+    }
+}
+
+public extension LottieView {
+    class Coordinator: NSObject {
+        var parent: LottieView
+    
+        init(_ animationView: LottieView) {
+            self.parent = animationView
+            super.init()
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+}
+
+struct LottieView_Previews: PreviewProvider {
+    static var previews: some View {
+        LottieView(name: "coffeeTime", play: .constant(true))
+            .frame(width: 500, height: 500, alignment: .center)
+    }
+}
+
--- Tests/LottieViewTests/LottieViewTests.swift
+++ Tests/LottieViewTests/LottieViewTests.swift
@@ -6,7 +6,7 @@ final class LottieViewTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LottieView().text, "Hello, World!")
+//        XCTAssertEqual(LottieView().text, "Hello, World!")
     }
 
     static var allTests = [
